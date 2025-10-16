b2c02e6f81320511685d02ddc919f356f8b47888
Charles D. Holmes <charles.d.holmes@gmail.com>
2025-10-16 17:14:42 -0500

    simplify

--- windows/Dwagoon.psm1
+++ windows/Dwagoon.psm1
@@ -14,25 +14,9 @@ function wal {
 
 function SetWallpaper() {
   param(
-    # Path to image to set as background, if not set current wallpaper is used
     [Parameter(Mandatory = $true)][string]$Image
   )
-
-  # Trigger update of wallpaper
-  # modified from https://www.joseespitia.com/2017/09/15/set-wallpaper-powershell-function/
-  Add-Type -TypeDefinition @"
-  using System;
-  using System.Runtime.InteropServices;
-
-  public class PInvoke
-  {
-    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
-    public static extern int SystemParametersInfo(UInt32 action, UInt32 iParam, String sParam, UInt32 winIniFlags);
-  }
-"@
-
-  # Setting the wallpaper requires an absolute path, so pass image into resolve-path
-  [PInvoke]::SystemParametersInfo(0x0014, 0, $($Image | Resolve-Path), 0x0003) | out-null
+  python -c "import pywal; pywal.wallpaper.change(r'$Image')"
 }
 
 function RandomWallpaper() {
