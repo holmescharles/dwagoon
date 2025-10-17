93554caa18ca0c0f33f129550b5082ad91f21641
Charles D. Holmes <charles.d.holmes@gmail.com>
2025-10-16 20:58:10 -0500

    working?

--- src/dwagoon/scripts/rice.py
+++ src/dwagoon/scripts/rice.py
@@ -6,7 +6,7 @@ import sys
 
 from ..message import message
 
-WALLPAPER_FOLDER = Path.home() / "Downloads" / "reddit"
+WALLPAPER_FOLDER = Path.home() / "Downloads" / "Wallpapers"
 
 
 def is_ssh():
@@ -24,13 +24,22 @@ def is_wsl():
     return False
 
 
+def is_windows():
+  return platform.system() == "Windows"
+
+
 def main():
   if is_ssh():
     message(f"{sys.argv[0]} will not run in SSH")
     return
 
   command = ["wal", "-i", WALLPAPER_FOLDER, "--backend", "colorz", "--cols16"] + sys.argv[1:]
+  message(f"Running: {' '.join(map(str, command))}")
   if is_wsl():
     message("Delegating to powershell")
     command = ["powershell.exe"] + command
   run(command)
+
+  if is_windows() and not is_wsl():
+    message("Updating terminal")
+    run(["powershell.exe", "-File", str(Path(__file__).parent / "UpdateTerminal.ps1")])
