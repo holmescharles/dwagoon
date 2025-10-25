4474b130d715de3166c955f406954abe4ca4caae
Charles D. Holmes <charles.d.holmes@gmail.com>
2025-10-24 21:02:49 -0500

    simplify rice

--- src/yodawg/rice.py
+++ src/yodawg/rice.py
@@ -18,21 +18,6 @@ PARSER.add_argument(
 )
 
 
-def is_ssh():
-  return bool(os.environ.get("SSH_CONNECTION"))
-
-
-def is_wsl():
-  if platform.system() != "Linux":
-    return False
-  try:
-    with open("/proc/version") as f:
-      info = f.read()
-    return "Microsoft" in info or "WSL" in info
-  except FileNotFoundError:
-    return False
-
-
 def is_windows():
   return platform.system() == "Windows"
 
@@ -40,20 +25,12 @@ def is_windows():
 def main():
   params = PARSER.parse_args()
 
-  if is_ssh():
-    message(f"{sys.argv[0]} will not run in SSH")
-    return
-
   command = ["wal", "-i", params.image, "--cols16"]
   if params.light:
     command += ["-l"]
-
   message(f"Running: {' '.join(map(str, command))}")
-  if is_wsl():
-    message("Delegating to powershell")
-    command = ["powershell.exe"] + command
   run(command)
 
-  if is_windows() and not is_wsl():
+  if is_windows():
     message("Updating terminal")
     run(["powershell.exe", "-File", str(Path(__file__).parent / "UpdateTerminal.ps1")])
